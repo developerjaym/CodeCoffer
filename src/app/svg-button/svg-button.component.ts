736fe8f33484ab4d1611a871621ba8d33473d86f
developerjaym <jaymansmann@yahoo.com>
2019-01-19 17:51:50 -0500

    A11y: added role and aria-label to svg button

--- src/app/svg-button/svg-button.component.ts
+++ src/app/svg-button/svg-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, Input } from '@angular/core';
+import { Component, OnInit, ElementRef, Input, Renderer2 } from '@angular/core';
 import { SvgService } from '../svg.service';
 
 @Component({
@@ -11,12 +11,17 @@ export class SvgButtonComponent implements OnInit {
   @Input("src")
   svgName: string;
 
+  @Input("label")
+  label?: string;  
 
-  constructor(private element: ElementRef, private svgService: SvgService) { }
+  constructor(private element: ElementRef, private renderer: Renderer2, private svgService: SvgService) { }
 
   ngOnInit() {
     this.svgService.getSvgByName(this.svgName).subscribe(newNode => {
       this.element.nativeElement.innerHTML = newNode;
+      this.renderer.setAttribute(this.element.nativeElement, "tabindex", "0");
+      this.renderer.setAttribute(this.element.nativeElement, "role", "button");
+      this.renderer.setAttribute(this.element.nativeElement, "aria-label", this.label || this.element.nativeElement.title);
     });
   }
 

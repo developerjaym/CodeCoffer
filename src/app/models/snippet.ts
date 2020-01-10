c7254cf4116698cdaa7d9ff84ef8d0fe96d9928e
jbhouse <jbmoorhouse1@gmail.com>
2020-01-09 19:08:46 -0500

    added backwards compatibility with legacy snippet model

--- src/app/models/snippet.ts
+++ src/app/models/snippet.ts
@@ -1,4 +1,5 @@
 import { Supplement } from './supplement';
+import { Language } from './language.enum';
 
 export class Snippet {
   id: string;
@@ -6,6 +7,9 @@ export class Snippet {
   title: string;
   index: string;
   tags: string;
+  code?: string; // deprecated
+  language?: string; // deprecated
+  notes?: string; // deprecated
   showing: boolean;
   pinned: boolean;
   supplements: Supplement[];
@@ -28,14 +32,32 @@ export class Snippet {
   }
 
   static createValidSnippet(snippet: Snippet): Snippet {
-    snippet.id = snippet.id || String(Math.floor(Math.random() * 1000000000) + 1);
-    snippet.timestamp = snippet.timestamp || Date.now();
-    snippet.title = snippet.title || '';
-    snippet.index = snippet.index || '';
-    snippet.tags = snippet.tags || '';
-    snippet.showing = snippet.showing === true || snippet.showing === false ? snippet.showing : false;
-    snippet.pinned = snippet.pinned === true || snippet.pinned === false ? snippet.pinned : false;
-    snippet.supplements = snippet.supplements || [new Supplement()];
+
+    if (snippet.code) {
+      snippet.id = snippet.id || String(Math.floor(Math.random() * 1000000000) + 1);
+      snippet.timestamp = snippet.timestamp || Date.now();
+      snippet.title = snippet.title || '';
+      snippet.index = snippet.index || '';
+      snippet.tags = snippet.tags || '';
+      snippet.showing = snippet.showing === true || snippet.showing === false ? snippet.showing : false;
+      snippet.pinned = snippet.pinned === true || snippet.pinned === false ? snippet.pinned : false;
+      snippet.supplements = [{ name: "supplement1", code: snippet.code, notes: snippet.notes, language: Language.TEXT }, ...snippet.supplements]
+      let i = 1;
+      snippet.supplements.map(supplement => {
+        supplement.name = "supplement" + i;
+        supplement.language = Language.TEXT;
+        i++;
+      });
+    } else {
+      snippet.id = snippet.id || String(Math.floor(Math.random() * 1000000000) + 1);
+      snippet.timestamp = snippet.timestamp || Date.now();
+      snippet.title = snippet.title || '';
+      snippet.index = snippet.index || '';
+      snippet.tags = snippet.tags || '';
+      snippet.showing = snippet.showing === true || snippet.showing === false ? snippet.showing : false;
+      snippet.pinned = snippet.pinned === true || snippet.pinned === false ? snippet.pinned : false;
+      snippet.supplements = snippet.supplements || [new Supplement()];
+    }
 
     return snippet;
   }

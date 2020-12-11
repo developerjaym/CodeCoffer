dd669f608a246bbd45dea7e715475d59554b0d91
Jay M <jaymansmann@yahoo.com>
2020-12-11 14:00:13 -0500

    www in url to prevent redirects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // url: "https://developerjaym.github.io"
-  url: "https://jayckers.com/snippet/",
+  url: "https://www.jayckers.com/snippet/",
   serverUrl: "https://jayman-gameserver.herokuapp.com/"
 };
